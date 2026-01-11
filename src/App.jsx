a5f182940446c243fcf4adab176a34a4bea8ca1b
lakshmiii1006-hub <lakshmiii1006@gmail.com>
2026-01-11 20:03:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react'
 import Navbar from './components/Navbar.jsx'
@@ -11,8 +10,6 @@ import Contact from './pages/Contact.jsx'
 import BookNow from './pages/BookNow.jsx'
 import Admin from './pages/Admin.jsx'
 
-
-
 export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-pink-50 to-rose-50">
@@ -20,18 +17,15 @@ export default function App() {
         <Navbar />
         <main className="pt-20">
           <Routes>
+            {/* PUBLIC ROUTES - Everyone can access */}
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/events" element={<Events />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/book" element={<BookNow />} /> {/* ✅ REMOVED SignedIn wrapper */}
             
-
-            <Route path="/book" element={
-              <SignedIn>
-                <BookNow />
-              </SignedIn>
-            } />
+            {/* PROTECTED ROUTE - Only logged-in admins */}
             <Route path="/admin" element={
               <SignedIn>
                 <Admin />
